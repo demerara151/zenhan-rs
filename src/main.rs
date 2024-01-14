a5ff6d7969f8f0d30c708aed07a8c39f038c2c47
demerara151 <demeraraonfire@gmail.com>
2024-01-14 14:44:53 +0900

    refactor:💄 reduce the amount of codes in unsafe block
    Hnadle arguments in safe Rust scope.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use windows::core::Result;
 use windows::Win32::Foundation::{LPARAM, WPARAM};
-use windows::Win32::UI::Input::Ime::{
-    ImmGetDefaultIMEWnd, IMC_SETOPENSTATUS, IMN_OPENSTATUSWINDOW,
-};
+use windows::Win32::UI::Input::Ime::{ImmGetDefaultIMEWnd, IMC_SETOPENSTATUS};
 use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, SendMessageA, WM_IME_CONTROL};
 
 fn main() -> Result<()> {
+    let set_open_status = WPARAM(IMC_SETOPENSTATUS.try_into()?);
+
+    let mode = get_input_mode();
+
     unsafe {
         let hwnd = GetForegroundWindow();
         let ime = ImmGetDefaultIMEWnd(hwnd);
 
-        let args: Vec<String> = std::env::args().collect();
-        if args.len() < 2 {
-            SendMessageA(
-                ime,
-                WM_IME_CONTROL,
-                WPARAM(IMN_OPENSTATUSWINDOW.try_into()?),
-                LPARAM(0),
-            );
-        } else {
-            let mode = args[1].parse().unwrap_or_default();
-            SendMessageA(
-                ime,
-                WM_IME_CONTROL,
-                WPARAM(IMC_SETOPENSTATUS.try_into()?),
-                LPARAM(mode),
-            );
-        }
+        SendMessageA(ime, WM_IME_CONTROL, set_open_status, mode);
     }
     Ok(())
+}
+
+fn get_input_mode() -> LPARAM {
+    let args: Vec<String> = std::env::args().collect();
+
+    // HACK: Consider using more proper way in Rust.
+    if args.len() < 2 {
+        LPARAM(0)
+    } else {
+        let mode = args[1].parse().unwrap_or_default();
+        LPARAM(mode)
+    }
 }
 
